54aa1393a87d7faf2512d6cba0fdb46cfd5ca98d
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2024-11-17 19:51:25 +0530

    2.py

---
+++ 2.py
@@ -0,0 +1,25 @@
+# This program adds two numbers
+num1 = 1.5
+num2 = 6.3
+# Add two numbers
+sum = num1 + num2
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+# The sum of 1.5 and 6.3 is 7.8
+
+# 2
+
+# Store input numbers
+num1 = input('Enter first number: ')
+num2 = input('Enter second number: ')
+# Add two numbers
+sum = float(num1) + float(num2)
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+# Enter first number: 4
+# Enter second number: 5
+# The sum of 4 and 5 is 9.0
+
+# 3
+
+print('The sum is %.1f' %(float(input('Enter first number: ')) + float(input('Enter second number: '))))
