2289d68fb1671f7560f62a5f1d72fc72038043d3
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2024-11-21 08:29:55 +0530

    Update 6.py

--- 6.py
+++ 6.py
@@ -22,3 +22,17 @@ x, y = y, x
 print("x =", x)
 print("y =", y)
 
+Addition and Subtraction
+x = x + y
+y = x - y
+x = x - y
+Multiplication and Division
+x = x * y
+y = x / y
+x = x / y
+XOR swap
+
+This algorithm works for integers only
+x = x ^ y
+y = x ^ y
+x = x ^ y
