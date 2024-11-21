3d441d1078349bfd3f239725d5cbe35f393d6f2d
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2024-11-21 08:28:59 +0530

    Update 6.py

--- 6.py
+++ 6.py
@@ -14,3 +14,11 @@ print('The value of x after swapping: {}'.format(x))
 print('The value of y after swapping: {}'.format(y))
 #The value of x after swapping: 10
 #The value of y after swapping: 5
+
+#2
+x = 5
+y = 10
+x, y = y, x
+print("x =", x)
+print("y =", y)
+
