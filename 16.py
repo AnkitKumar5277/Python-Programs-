577f18992be33695a376765a4359d71164453c90
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2024-12-02 11:33:09 +0530

    Update 16.py

--- 16.py
+++ 16.py
@@ -15,3 +15,31 @@ else:
        factorial = factorial*i
    print("The factorial of",num,"is",factorial)
    # The factorial of 7 is 5040
+
+# iteration	factorial*i (returned value)
+# i = 1	1 * 1 = 1
+# i = 2	1 * 2 = 2
+# i = 3	2 * 3 = 6
+# i = 4	6 * 4 = 24
+# i = 5	24 * 5 = 120
+# i = 6	120 * 6 = 720
+# i = 7	720 * 7 = 5040
+
+# Python program to find the factorial of a number provided by the user
+# using recursion
+def factorial(x):
+    """This is a recursive function
+    to find the factorial of an integer"""
+    if x == 1 or x == 0:
+        return 1
+    else:
+        # recursive call to the function
+        return (x * factorial(x-1))
+# change the value for a different result
+num = 7
+# to take input from the user
+num = int(input("Enter a number: "))
+# call the factorial function
+result = factorial(num)
+print("The factorial of", num, "is", result)
+
