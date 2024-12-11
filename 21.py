709509d0aca09b7c770da2ce56221576790617e4
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2024-12-12 01:19:45 +0530

    Update 21.py

--- 21.py
+++ 21.py
@@ -11,3 +11,7 @@ else:
        num -= 1
    print("The sum is", sum)
    # The sum is 136
+
+# Write a function to find the sum of first N natural numbers.
+# Hint: The formula for the sum of the first N natural numbers is N*(N+1)/2.
+# For example, for input 5, the outout should be 15.
