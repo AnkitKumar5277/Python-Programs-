e50cf4c32f8f406318251bafe647a87eca421ba9
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2024-12-17 17:54:35 +0530

    Update 23.py

--- 23.py
+++ 23.py
@@ -5,3 +5,5 @@ my_list = [12, 65, 54, 39, 102, 339, 221,]
 result = list(filter(lambda x: (x % 13 == 0), my_list))
 # display the result
 print("Numbers divisible by 13 are",result)
+# output
+# Numbers divisible by 13 are [65, 39, 221]
