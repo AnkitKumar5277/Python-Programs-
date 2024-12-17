89de33b246794f045f0d512f04b29524010b8bf1
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2024-12-17 17:57:16 +0530

    Update 23.py

--- 23.py
+++ 23.py
@@ -7,3 +7,15 @@ result = list(filter(lambda x: (x % 13 == 0), my_list))
 print("Numbers divisible by 13 are",result)
 # output
 # Numbers divisible by 13 are [65, 39, 221]
+
+# challenge
+# Write a function to check if a number is divisible by five.
+# Return True if the number is divisible by 5. Otherwise, return False.
+def is_divisible_by_five(number):
+    return number % 5 == 0
+# Example usage
+print(is_divisible_by_five(10))  # True
+print(is_divisible_by_five(12))  # False
+print(is_divisible_by_five(0))   # True
+print(is_divisible_by_five(-15)) # True
+
