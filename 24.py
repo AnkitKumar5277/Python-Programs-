62e34f1d260acb7b43a2ae0e9ce48750dd9f3e4f
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2024-12-18 18:27:01 +0530

    Update 24.py

--- 24.py
+++ 24.py
@@ -1,6 +1,6 @@
 # Python Program to Convert Decimal to Binary, Octal and Hexadecimal
 # Python program to convert decimal into other number systems
-dec = 344
+dec = int(input("Enter No."))
 print("The decimal value of", dec, "is:")
 print(bin(dec), "in binary.")
 print(oct(dec), "in octal.")
