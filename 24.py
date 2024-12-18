1700d84eb77325358a2a2aebede78743c5541209
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2024-12-18 18:24:54 +0530

    Update 24.py

--- 24.py
+++ 24.py
@@ -5,3 +5,8 @@ print("The decimal value of", dec, "is:")
 print(bin(dec), "in binary.")
 print(oct(dec), "in octal.")
 print(hex(dec), "in hexadecimal.")
+# output
+# The decimal value of 344 is:
+# 0b101011000 in binary.
+# 0o530 in octal.
+# 0x158 in hexadecimal.
