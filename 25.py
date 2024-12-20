133f38a1afa05b8ba5267947942c6d3d8a49b8e6
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2024-12-20 13:05:52 +0530

    Update 25.py

--- 25.py
+++ 25.py
@@ -1,2 +1,6 @@
 # Python Program to Find ASCII Value of Character
 # ASCII stands for American Standard Code for Information Interchange.
+# Program to find the ASCII value of the given character
+c = 'p'
+print("The ASCII value of '" + c + "' is", ord(c))
+
