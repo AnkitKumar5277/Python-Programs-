2e5852bd9dc282cbec769a1f16c64085011619ff
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2025-01-01 13:00:38 +0530

    Update 33.py

--- 33.py
+++ 33.py
@@ -1,1 +1,11 @@
-Python Program to Find Sum of Natural Numbers Using Recursion
+# Python Program to Find Sum of Natural Numbers Using Recursion
+def sum(n):
+  if n<=1:
+    return n
+  else
+    return n + sum(n-1)
+num = 16
+if num < 0:
+   print("Enter a positive number")
+else:
+   print("The sum is",recur_sum(num))
