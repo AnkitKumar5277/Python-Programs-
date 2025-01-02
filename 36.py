82fc66d3789dbe380b97b1969b5486d4cff43e01
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2025-01-02 12:28:55 +0530

    Update 36.py

--- 36.py
+++ 36.py
@@ -16,4 +16,17 @@ for i in range(len(X)):
        result[i][j] = X[i][j] + Y[i][j]
 for r in result:
    print(r)
+# [17, 15, 4]
+# [10, 12, 9]
+# [11, 13, 18]
 
+# Source Code: Matrix Addition using Nested List Comprehension
+X = [[12,7,3],
+    [4 ,5,6],
+    [7 ,8,9]]
+Y = [[5,8,1],
+    [6,7,3],
+    [4,5,9]]
+result = [[x[i][j]+y[i][j] for j in range(len(x[0]))] for i in range(len(x))]
+for r in result:
+    print(r)
