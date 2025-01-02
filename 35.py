ede4582aaaacd161236e76ff22c934578e9c723e
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2025-01-02 12:23:13 +0530

    Create 35.py

---
+++ 35.py
@@ -0,0 +1,8 @@
+# Python Program to Convert Decimal to Binary Using Recursion
+def converttobinary(n):
+  if n > 1:
+    converttobinary(n//2)
+  print(n%2, end = '')
+dec = 34
+converttobinary(dec)
+print()
