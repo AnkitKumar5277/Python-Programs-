02fb7441893f1241d315c93d8e60dff4f893d89b
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2025-01-04 17:34:50 +0530

    Update 38.py

--- 38.py
+++ 38.py
@@ -19,4 +19,15 @@ for r in result:
 # [74, 97, 73, 14]
 # [119, 157, 112, 23]
 
+# Program to multiply two matrices using list comprehension
+X = [[12,7,3],
+    [4 ,5,6],
+    [7 ,8,9]]
+Y = [[5,8,1,2],
+    [6,7,3,0],
+    [4,5,9,1]]
+result = [[sum(a*b for a,b in zip(x_row, y_col)) for y_col in zip(*y)] for x_row in x]
+for r in result:
+     print(r)
+     
 
