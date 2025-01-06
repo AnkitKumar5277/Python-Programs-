cb1ee6511283efa787e868f61c0db18af750d852
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2025-01-06 15:18:12 +0530

    Update 40.py

--- 40.py
+++ 40.py
@@ -7,8 +7,3 @@ for char in my_str:
     no_punct = no_punct + char
 print(no_punct)
 # Hello he said and went
-# # output
-# Union of E and N is {0, 1, 2, 3, 4, 5, 6, 8}
-# Intersection of E and N is {2, 4}
-# Difference of E and N is {8, 0, 6}
-# Symmetric difference of E and N is {0, 1, 3, 5, 6, 8}
