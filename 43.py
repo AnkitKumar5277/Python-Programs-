5693b21f404efcfdb8a672ea02e819c0a34d0ef7
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2025-01-07 16:20:43 +0530

    Update 43.py

--- 43.py
+++ 43.py
@@ -1,11 +1,20 @@
 # Python Program to Count the Number of Each Vowel
 # Program to count the number of each vowels
 vowels = 'aeiou'
-ip_str = 'Hello, have you tried our tutorial section yet?'
+ip_str = 'Hello, have you tried our tutorial section yet?
 ip_str = ip_str.casefold()
 count = {}.fromkeys(vowels,0)
 for char in ip_str:
-   if char in count:
-       count[char] += 1
+   if char in
+      count[char] += 1
 print(count)
+# output
+# {'a': 2, 'e': 5, 'i': 3, 'o': 5, 'u': 3}
+
+# Source Code: Using a list and a dictionary comprehension
+ip_str = 'Hello, have you tried our tutorial section yet?'
+ip_str = ip_str.casefold()
+count = {x:sum([1 for char in ip_str if char == x]) for x in 'aeiou'}
+print(count)
+# {'a': 2, 'e': 5, 'i': 3, 'o': 5, 'u': 3}
 
