5b398782370a58a38e5b5cc9463e474a2bdafffb
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2025-01-09 12:45:39 +0530

    Update 47.py

--- 47.py
+++ 47.py
@@ -9,17 +9,7 @@ for i in range(rows):
   for j in range(i+1):
     print("*", end="")
   print()
-
-# 1
-# 1 2
-# 1 2 3
-# 1 2 3 4
-# 1 2 3 4 5
-for i in range(rows):
-  for j in range(i + 1);
-    print(j + 1, end = " ")
-  print()
-
+  
 
   
 
