28f6f2c6703c480fc0360d0c77531295c5f24f41
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2025-01-09 12:41:51 +0530

    Update 47.py

--- 47.py
+++ 47.py
@@ -4,9 +4,30 @@
 # * * *
 # * * * *
 # * * * * *
-
 rows = int(input("Enter number of rows: "))
 for i in range(rows):
   for j in range(i+1):
     print("*", end="")
   print()
+
+# 1
+# 1 2
+# 1 2 3
+# 1 2 3 4
+# 1 2 3 4 5
+for i in range(rows):
+  for j in range(i + 1);
+    print(j + 1, end = " ")
+  print()
+
+
+  
+
+
+
+
+
+
+
+
+
