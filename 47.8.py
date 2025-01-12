34e3295cf809e78aa2e1a803fcb4f71425c1bb56
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2025-01-12 13:14:07 +0530

    Update 47.8.py

--- 47.8.py
+++ 47.8.py
@@ -4,4 +4,17 @@
  #     1   3   3    1
  #   1  4    6   4   1
  # 1  5   10   10  5   1
-rows = 
+
+rows = int(input("enter :")
+coef = 1
+for i in range(1, rows+1):
+ for space in range(1, rows-i+1):
+  print(" ", end = "")
+ for j in range(0, i):
+  if j == 0 or i ==0:
+   coef = 1
+  else:
+   coef = coef * (i - j)//j
+  print(coef, end = " ")
+ print()
+ 
