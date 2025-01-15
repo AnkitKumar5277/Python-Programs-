d67b6105c025e0ab436679074ad1ae4cc485a7fa
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2025-01-15 13:44:05 +0530

    Update 49.py

--- 49.py
+++ 49.py
@@ -3,3 +3,8 @@
 # Example 1: Using pathlib.Path.mkdir
 from pathlib import Path
 Path("/root/dirA/dirB").mkdir(parents=True, exist_ok=True)
+
+# Example 2: Using os.makedirs
+import os
+os.makedirs("/root/dirA/dirB")
+
