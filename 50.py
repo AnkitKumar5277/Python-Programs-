d5fec6923f36a50a2b40bb35048448ce4124425f
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2025-01-16 11:13:33 +0530

    Update 50.py

--- 50.py
+++ 50.py
@@ -10,4 +10,19 @@ for index, val in enumerate(l):
 # 2 35
 # 3 11
 
-Example 2: Start the indexing with non zero value
+# Example 2: Start the indexing with non zero value
+l  = [21, 44, 35, 11]
+for index, val in enumerate(l, start = 1):
+    print(index, val)
+# Output
+# 1 21
+# 2 44
+# 3 35
+# 4 11
+
+# Example 3: Without using enumerate()
+l = [21, 44, 35, 11]
+for index in range(len(l)):
+    value = l[index]
+    print(index, value)
+
