202232ae7ce556744d4663b22973cc0cf933778e
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2025-02-02 17:02:12 +0530

    Update 51.py

--- 51.py
+++ 51.py
@@ -1,2 +1,11 @@
 # Python Program to Flatten a Nested List
 # Example 1: Using List Comprehension
+my_list = [[1],[2,3],[4,5,6,7]]
+flat_list = [num for sublist in my_list for num in sublist]
+print(flat_list)
+# [1, 2, 3, 4, 5, 6, 7]
+
+# Example 2: Using Nested for Loops (non pythonic way)
+
+
+
