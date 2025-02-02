05dcf993e3cad7413e35918e2a09ae99d7be2ace
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2025-02-02 17:03:26 +0530

    Update 51.py

--- 51.py
+++ 51.py
@@ -6,6 +6,11 @@ print(flat_list)
 # [1, 2, 3, 4, 5, 6, 7]
 
 # Example 2: Using Nested for Loops (non pythonic way)
-
+my_list = [[1],[2,3],[4,5,6,7]]
+flat_list[]
+for sublist in my_list:
+  for num in sublist:
+    flat_list.append(num)
+print(flat_list)
 
 
