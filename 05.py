af28f15676517131b15179eb1ff1759d95840217
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2025-02-12 13:11:32 +0530

    Update 05.py

--- 05.py
+++ 05.py
@@ -31,7 +31,6 @@ def direction(number):
         return 'Zero'
 # print(direction(5))  # Output: 'Up'
 
-
 # Python program to check if the input number is odd or even.
 # A number is even if division by 2 gives a remainder of 0.
 # If the remainder is 1, it is an odd number.
@@ -40,7 +39,6 @@ if (num % 2) == 0:
    print("{0} is Even".format(num))
 else:
    print("{0} is Odd".format(num))
-
 # Enter a number: 43
 # 43 is Odd
 # Enter a number: 18
@@ -49,13 +47,6 @@ else:
 # Challenge:
 # Write a function to check if the entered integer is odd or even.
 def check_odd_or_even(number):
-    """
-    Determines if a given number is odd or even.
-    Args:
-        number (int): The number to be checked.
-    Returns:
-        str: "Odd" if the number is odd, "Even" if the number is even.
-    """
     if number % 2 == 0:
         return "Even"
     else:
@@ -63,5 +54,3 @@ def check_odd_or_even(number):
 # Example usage:
 print(check_odd_or_even(4))  # Output: Even
 print(check_odd_or_even(7))  # Output: Odd
-
-
