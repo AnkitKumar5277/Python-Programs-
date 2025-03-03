0e4593d3af486e6f9b4cf3c96bf3dd6aeb062d86
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2025-03-03 13:59:36 +0530

    Update 02.py

--- 02.py
+++ 02.py
@@ -27,3 +27,25 @@ base = 3
 height = 4
 print(area_of_triangle(base, height))
 
+# Python Program to Find Armstrong Number in an Interval
+# Program to check Armstrong numbers in a certain interval
+lower = 100
+upper = 2000
+for num in range(lower, upper + 1):
+   # order of number
+   order = len(str(num))
+   # initialize sum
+   sum = 0
+   temp = num
+   while temp > 0:
+       digit = temp % 10
+       sum += digit ** order
+       temp //= 10
+   if num == sum:
+       print(num)
+# #output
+# 153
+# 370
+# 371
+# 407
+# 1634
