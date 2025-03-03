c1eaff87756f32a305f9cd193dfb7261cf56460c
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2025-03-03 14:01:56 +0530

    Update 04.py

--- 04.py
+++ 04.py
@@ -19,3 +19,27 @@ for i in range(1, 11):
 # 12 x 9 = 108
 # 12 x 10 = 120
 
+# Python Program to Find HCF or GCD
+def compute_hcf(x,y):
+  if x>y:
+    smaller = y
+  else:
+    smaller = x
+  for i in range(1, smaller+1):
+    if((x%i==0) and (y%i==0)):
+      hcf = i
+      return hcf
+num1 = 54
+num2 = 24
+print("the h.c.f. is", compute_hcf(num1, num2))
+# output
+# The H.C.F. is 6
+
+# Source Code: Using the Euclidean Algorithm
+def compute_hcf(x,y):
+  while(y):
+    x,y = x&y
+  return x
+hcf = compute_hcf(300, 400)
+print("the hcf is", hcf)
+# The HCF is 100
