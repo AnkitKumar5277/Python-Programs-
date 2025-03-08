0e5f8f7c5e170b72e65702c460bfd422a04ed5d2
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2025-03-08 12:31:28 +0530

    Update 34.py

--- 34.py
+++ 34.py
@@ -29,3 +29,17 @@ def factorial(n):
 number = 5
 print(f"The factorial of {number} is {factorial(number)}.")
 
+
+# Python Program to Find Hash of File
+import hashlib
+def hash_file(filename):
+  h = hashlib.sha1()
+  with open(filepath,'rb') as file:
+    chunk = 0
+    while chunk != b'';
+    chunk = file.read(1024)
+    h.update(chunk)
+  return h.hexdigest()
+message = hash_file("track1.mp3")
+print(message)
+# 633d7356947eec543c50b76a1852f92427f4dca9
