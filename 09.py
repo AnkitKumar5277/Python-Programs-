d64e6c22e8386208ca0d920c1c45d2b2f31911b9
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2025-03-08 12:29:14 +0530

    Update and rename 17.py to 09.py

--- 09.py
+++ 09.py
@@ -47,3 +47,24 @@ def lcm(a, b):
 print(lcm(12, 15))  # Output: 60
 
 
+# Python Program to Convert Decimal to Binary, Octal and Hexadecimal
+# Python program to convert decimal into other number systems
+dec = int(input("Enter No."))
+print("The decimal value of", dec, "is:")
+print(bin(dec), "in binary.")
+print(oct(dec), "in octal.")
+print(hex(dec), "in hexadecimal.")
+
+# output
+# The decimal value of 344 is:
+# 0b101011000 in binary.
+# 0o530 in octal.
+# 0x158 in hexadecimal.
+
+# Python Program to Find ASCII Value of Character
+# ASCII stands for American Standard Code for Information Interchange.
+# Program to find the ASCII value of the given character
+c = 'p'
+print("The ASCII value of '" + c + "' is", ord(c))
+# output
+# The ASCII value of 'p' is 112
