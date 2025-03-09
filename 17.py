738f267660a3178d779d6cd5581a5e4397760ea4
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2025-03-09 14:17:26 +0530

    Update and rename 20.py to 17.py

--- 17.py
+++ 17.py
@@ -17,3 +17,21 @@ for i in range(5):
 # 8 of Spade
 # 12 of Spade
 # 4 of Spade
+
+# Python Program to Flatten a Nested List
+# Example 1: Using List Comprehension
+my_list = [[1],[2,3],[4,5,6,7]]
+flat_list = [num for sublist in my_list for num in sublist]
+print(flat_list)
+# [1, 2, 3, 4, 5, 6, 7]
+
+# Example 2: Using Nested for Loops (non pythonic way)
+my_list = [[1],[2,3],[4,5,6,7]]
+flat_list[]
+for sublist in my_list:
+  for num in sublist:
+    flat_list.append(num)
+print(flat_list)
+# [1, 2, 3, 4, 5, 6, 7]
+
+
