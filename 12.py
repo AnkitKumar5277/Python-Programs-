eed558de6d03281401371de18ccb8e874c53fc99
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2025-03-09 14:18:04 +0530

    Update 12.py

--- 12.py
+++ 12.py
@@ -32,3 +32,36 @@ print(calendar.month(yy, mm))
 # 10 11 12 13 14 15 16
 # 17 18 19 20 21 22 23
 # 24 25 26 27 28 29 30
+
+# Python Program to Access Index of a List Using for Loop
+
+# Example 1: Using enumerate
+l = [21, 44, 35, 11]
+for index, val in enumerate(l):
+    print(index, val)
+#     # Output
+# 0 21
+# 1 44
+# 2 35
+# 3 11
+
+# Example 2: Start the indexing with non zero value
+l  = [21, 44, 35, 11]
+for index, val in enumerate(l, start = 1):
+    print(index, val)
+# Output
+# 1 21
+# 2 44
+# 3 35
+# 4 11
+
+# Example 3: Without using enumerate()
+l = [21, 44, 35, 11]
+for index in range(len(l)):
+    value = l[index]
+    print(index, value)
+# Output
+# 0 21
+# 1 44
+# 2 35
+# 3 11
