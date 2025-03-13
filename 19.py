fc64ff1cb21f2db70d8ce8a0a36b5f9b22c593a6
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2025-03-13 11:48:00 +0530

    Update 19.py

--- 19.py
+++ 19.py
@@ -1,3 +1,17 @@
+# Example 5: Inverted half pyramid using numbers
+# 1 2 3 4 5
+# 1 2 3 4
+# 1 2 3
+# 1 2
+# 1
+rows = int(input("enter number of rows:"))
+for i in range(rows, 0, -1):
+  for j in range(1, i+1):
+    print(j, end=" ")
+
+  print()
+  
+
 # Python Program to Check Whether a String is Palindrome or Not
 my_str='aIbohPhoBiA'
 my_str = my_str.casefold()
