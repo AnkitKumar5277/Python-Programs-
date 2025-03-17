56cf71f7d8fe692fb46b03fe40c8a360538aa41a
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2025-03-17 14:48:54 +0530

    Update 17.py

--- 17.py
+++ 17.py
@@ -1,3 +1,11 @@
+# Example 3: Using copy() and update()
+d1 = {1: 'a', 2: 'b'}
+d2 = {2: 'c', 4: 'd'}
+d3 = d2.copy()
+d3.update(d1)
+print(d3)
+# {2: 'b', 4: 'd', 1: 'a'}
+
 # Python Program to Shuffle Deck of Cards
 # Python program to shuffle a deck of card
 # importing modules
