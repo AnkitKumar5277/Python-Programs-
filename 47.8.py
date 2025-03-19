df08dc5a7da9f339fbf83bdf79e1ae907f3c67d5
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2025-03-19 13:49:59 +0530

    Update 47.8.py

--- 47.8.py
+++ 47.8.py
@@ -18,3 +18,8 @@ for i in range(1, rows+1):
   print(coef, end = " ")
  print()
  
+# Example 2: Using the ** Operator
+d1 = {1: 'a', 2: 'b'}
+d2 = {2: 'c', 4: 'd'}
+print({**d1, **d2})
+# {1: 'a', 2: 'c', 4: 'd'}
