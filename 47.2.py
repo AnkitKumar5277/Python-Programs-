1807485a817737f89cbc57f8513d8d133a056ec5
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2025-03-20 12:07:16 +0530

    Update 47.2.py

--- 47.2.py
+++ 47.2.py
@@ -11,3 +11,17 @@ for i in range(rows):
     print(alphabet, end=" ")
   ascii_value += 1
   print()
+
+# Programs to print inverted half pyramid using * and numbers
+# Example 4: Inverted half pyramid using *
+# * * * * *
+# * * * *
+# * * *
+# * *
+# *
+input = int(input("Enter")
+for i in range(input, 0, -1);
+  for j in range(0, i):
+    print("* ", end = " ")
+
+  print()
