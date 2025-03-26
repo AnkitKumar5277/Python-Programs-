c15312b4990b3a486dc6f61284c427c76a1ad2ce
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2025-03-26 20:34:46 +0530

    Update filter() function.py

--- filter() function.py
+++ filter() function.py
@@ -36,3 +36,26 @@ print(evens)
 pairs = [(1, 2), (4, 1), (3, 3)]
 sort = sorted(pairs, key = lambda x: x[1])
 print(sort)
+
+# map function
+# map(function, iterable, ...)
+num = [1, 2, 3, 4, 5]
+square = map(lambda x: x**2, num)
+print(list(square))
+
+names = ['john', 'sam', 'linda']
+upper_names = map(str.upper, names)
+print(list(upper_names))
+
+a = [1, 2, 3]
+b = [4, 5, 6]
+sum_ab = map(lambda x, y: x + y, a, b)
+print(list(sum_ab))
+
+string_numbers = ['1', '2', '3']
+integers = map(int, string_numbers)
+print(list(integers))
+
+words = ["python", "data", "science", "analytics"]
+lengths = map(len, words)
+print(list(lengths))
