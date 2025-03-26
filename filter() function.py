3e7deaa1e57140cb9ce4cc69771fcf51d826b270
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2025-03-26 20:35:23 +0530

    Update filter() function.py

--- filter() function.py
+++ filter() function.py
@@ -59,3 +59,24 @@ print(list(integers))
 words = ["python", "data", "science", "analytics"]
 lengths = map(len, words)
 print(list(lengths))
+
+from functools import reduce
+
+# reduce(function, iterable[, initializer])
+from functools import reduce
+
+numbers = [1, 2, 3, 4, 5]
+result = reduce(lambda x, y: x + y, numbers)
+print(result)  # Output: 15
+
+num = [1,2,3,4,5]
+result = reduce(lambda x,y: x * y, num)
+print(result)
+
+num = [3,7,2,8,5]
+max = reduce(lambda x, y: x if x > y else y, num)
+print(max)
+
+num = [1,2,3,4,5]
+result = reduce(lambda x, y: x+y, num, 10)
+print(result)
