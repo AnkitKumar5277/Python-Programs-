cc413daeaa39ae90ea5b51d52f6159f5d808efcf
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2025-04-01 21:03:24 +0530

    Update 04.py

--- 04.py
+++ 04.py
@@ -1,3 +1,55 @@
+# Python Program to calculate the square root
+num = float(input('Enter a number: '))
+num_sqrt = num ** 0.5
+print('The square root of %0.3f is %0.3f'%(num ,num_sqrt))
+
+# Find square root of real or complex numbers
+# Importing the complex math module
+import cmath
+num = 1+2j
+#num = eval(input('Enter a number: '))
+num_sqrt = cmath.sqrt(num)
+print('The square root of {0} is {1:0.3f}+{2:0.3f}j'.format(num ,num_sqrt.real,num_sqrt.imag))
+
+# Python Program to find the area of triangle
+a = float(input('Enter first side: '))
+b = float(input('Enter second side: '))
+c = float(input('Enter third side: '))
+s = (a + b + c) / 2
+area = (s*(s-a)*(s-b)*(s-c)) ** 0.5
+print('The area of the triangle is %0.2f' %area)
+
+# 2 practice
+def area_of_triangle(base, height):
+    area = 0.5 * base * height
+    return round(area, 2)
+base = 3
+height = 4
+print(area_of_triangle(base, height))
+
+# Python Program to Find Armstrong Number in an Interval
+# Program to check Armstrong numbers in a certain interval
+lower = 100
+upper = 2000
+for num in range(lower, upper + 1):
+   # order of number
+   order = len(str(num))
+   # initialize sum
+   sum = 0
+   temp = num
+   while temp > 0:
+       digit = temp % 10
+       sum += digit ** order
+       temp //= 10
+   if num == sum:
+       print(num)
+# #output
+# 153
+# 370
+# 371
+# 407
+# 1634
+
 while True:
   n1 = int(input("enter: "))
   n2 = int(input("enter: "))
