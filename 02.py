7dee2b58c8b504649b5184245636ecd9411645e2
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2025-12-16 20:56:28 +0530

    Update 02.py

--- 02.py
+++ 02.py
@@ -1,52 +1,69 @@
-# Python Program to Solve Quadratic Equation
-# The standard form of a quadratic equation is:
-# ax2 + bx + c = 0, where
-# a, b and c are real numbers and
-# a ≠ 0
-# The solutions of this quadratic equation is given by:
-# (-b ± (b ** 2 - 4 * a * c) ** 0.5) / (2 * a)
-# Solve the quadratic equation ax**2 + bx + c = 0
-# import complex math module
+
+num1 = 1.5
+num2 = 6.3
+sum = num1 + num2
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+
+num1 = input('Enter first number: ')
+num2 = input('Enter second number: ')
+sum = float(num1) + float(num2)
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+print('The sum is %.1f' %(float(input('Enter first number: ')) + float(input('Enter second number: '))))
+
+kilometers = float(input("Enter value in kilometers: "))
+conv_fac = 0.621371
+miles = kilometers * conv_fac
+print('%0.2f kilometers is equal to %0.2f miles' %(kilometers,miles))
+
+celsius = float(input("Enter: "))
+fahrenheit = (celsius * 1.8) + 32
+print('%0.1f degree Celsius is equal to %0.1f degree Fahrenheit' %(celsius,fahrenheit))
+
+import random 
+print(random.randint(0,9))  #random.randint(a,b)
+
+my_str = "Hello this Is an Example With cased letters"
+words = [word.lower() for word in my_str.split()]
+words.sort()
+print("the sorted words are:")
+for word in words:
+  print(word)
+# # ouput
+# The sorted words are:
+# an
+# cased
+# example
+# hello
+# is
+# letters
+# this
+# with
+
 import cmath
 a = 1
 b = 5
 c = 6
-# calculate the discriminant
 d = (b**2) - (4*a*c)
 sol1 = (-b-cmath.sqrt(d))/(2*a)
 sol2 = (-b+cmath.sqrt(d))/(2*a)
 print('The solution are {0} and {1}'.format(sol1,sol2))
-# Enter a: 1
-# Enter b: 5
-# Enter c: 6
 # The solutions are (-3+0j) and (-2+0j)
 
-# challenge
 import cmath  
 def solve_quadratic(a, b, c):
-    # Calculate the discriminant
     discriminant = b**2 - 4*a*c
-    # Calculate the two solutions using the quadratic formula
     root1 = (-b + cmath.sqrt(discriminant)) / (2 * a)
     root2 = (-b - cmath.sqrt(discriminant)) / (2 * a)
-    # Return the roots as a list, ensuring root1 appears first
     return [root1, root2]
 
-
-# Python Program to Swap Two Variables
-# Python program to swap two variables
 x = input('Enter value of x: ')
 y = input('Enter value of y: ')
-# create a temporary variable and swap the values
 temp = x
 x = y
 y = temp
 print('The value of x after swapping: {}'.format(x))
 print('The value of y after swapping: {}'.format(y))
-#The value of x after swapping: 10
-#The value of y after swapping: 5
 
-#2
 x = 5
 y = 10
 x, y = y, x
